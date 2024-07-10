0aab3e0faf41722b151dbfbb712b22b63924956b
viet34tqc <viet34tqc@gmail.com>
2024-07-10 23:25:52 +0700

    Setup config service

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const serverPort = configService.get('SERVER_PORT');
+  app.enableCors({ origin: configService.get('CLIENT_URL') });
+  await app.listen(serverPort);
 }
 bootstrap();
