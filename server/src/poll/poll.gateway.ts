f102cf34d0035c51a3a3567cefd5e8dee1b69360
viet34tqc <viet34tqc@gmail.com>
2024-07-30 23:57:20 +0700

    Implement socket io in poll gateway

--- server/src/poll/poll.gateway.ts
+++ server/src/poll/poll.gateway.ts
@@ -1,11 +1,37 @@
 import { Logger } from '@nestjs/common';
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Namespace, Socket } from 'socket.io';
+import { PollService } from './poll.service';
 
 @WebSocketGateway()
-export class PollGateway {
+export class PollGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   private readonly logger = new Logger(PollGateway.name);
-  // Gateway initialized (provided in module and instantiated)
+  constructor(private readonly pollService: PollService) {}
+
+  // We are using socket io namespace to allow multiple polls using single connection
+  @WebSocketServer()
+  io: Namespace;
+
+  // These method is required when we implement those lifecycle hooks above
   afterInit(): void {
     this.logger.log(`Websocket Gateway initialized.`);
+  }
+  handleConnection(client: Socket) {
+    const sockets = this.io.sockets;
+    this.logger.log(`Client connected: ${client.id}`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+  }
+  handleDisconnect(client: Socket) {
+    const sockets = this.io.sockets;
+    this.logger.log(`Client disconected: ${client.id}`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
   }
 }
