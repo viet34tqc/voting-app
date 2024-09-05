9dedbe4a218d27367e15f8675fe281ac3e1d410b
viet34tqc <viet34tqc@gmail.com>
2024-09-05 23:53:57 +0700

    Clear query if error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useEffect } from 'react'
 import Layout from './components/layout'
 import Reconnect from './components/reconnect'
 import { Toaster } from './components/ui/toast/toaster'
 import Steps from './steps'
-import { useAppStore } from './stores/app-store'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -16,10 +14,6 @@ const queryClient = new QueryClient({
 })
 
 function App() {
-  const initSocket = useAppStore.initSocket()
-  useEffect(() => {
-    initSocket()
-  }, [])
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
--- client/src/components/reconnect/index.ts
+++ client/src/components/reconnect/index.ts
@@ -1,8 +1,10 @@
 import { useGetMe } from '@/queries/use-get-me'
 import { useAppStore } from '@/stores/app-store'
+import { useQueryClient } from '@tanstack/react-query'
 import { useEffect } from 'react'
 
 const Reconnect = () => {
+  const queryClient = useQueryClient()
   const { data: user, error } = useGetMe()
   const currentPoll = useAppStore.poll()
   const setCurrentStep = useAppStore.setCurrentStep()
@@ -11,6 +13,11 @@ const Reconnect = () => {
     // There might be error when database is clear but user still has accessToken
     if (error) {
       localStorage.removeItem('accessToken')
+      // Need to remove query from cache, otherwise when we remove poll from database
+      // Refresh the page, useGetMe is invoked and returns error.
+      // Now, try to create new one immediately, the query cache returns error again and it runs into this block
+      // And the access token is cleared immediately after it is created
+      queryClient.removeQueries({ queryKey: ['me'] })
       return
     }
     // If no error, but also no user, that means there is no access token
