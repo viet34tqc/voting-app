fa7eb398d9ee0e6eb3c1f376313ef1bf5748cf26
viet34tqc <viet34tqc@gmail.com>
2024-09-23 23:52:56 +0700

    Fix lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 const config = {
   'client/**/*.{js,jsx,ts,tsx}': ['pnpm --filter client lint', 'pnpm format'],
-  'server/**/.{js,jsx,ts,tsx}': ['pnpm --filter server lint', 'pnpm format'],
+  'server/**/*.{js,ts}': ['pnpm --filter server lint', 'pnpm format'],
 }
 export default config
