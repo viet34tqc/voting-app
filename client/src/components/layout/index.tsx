99e562051ee13a3260a230c7c51bb34c9436f608
viet34tqc <viet34tqc@gmail.com>
2024-10-02 16:15:19 +0700

    Fix layout on mobile

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -2,7 +2,9 @@ import { PropsWithChildren } from 'react'
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <main className='bg-green-50 flex items-center justify-center min-h-[100svh]'>{children}</main>
+    <main className='bg-green-50 flex items-center justify-center min-h-[100svh] px-4'>
+      {children}
+    </main>
   )
 }
 
